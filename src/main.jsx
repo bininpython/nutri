876c9201e40a350354a962d50ba5871688e0423f
bininpython <142698210+bininpython@users.noreply.github.com>
2026-08-27 04:32:08 -0300

    fix: load final layout stability layer

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './styles.css'
 import './visual-system.css'
+import './layout-stability.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
